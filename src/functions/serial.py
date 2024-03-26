7459e5b52957024f360147e5a6cff7f512655185
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-26 19:38:00 +0000

    Add src/functions/serial.py

---
+++ src/functions/serial.py
@@ -0,0 +1,33 @@
+"""
+Module serial.py
+"""
+import yaml
+
+
+class Serial:
+    """
+    Class Serial
+
+    Description
+    -----------
+    Presently, this class reads-in local YAML data files; YAML is a data serialisation language.
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+    @staticmethod
+    def get_dictionary(uri: str) -> dict:
+        """
+
+        :param uri: The file string of a local YAML file; path + file name + extension.
+        :return:
+        """
+
+        with open(file=uri, mode='r', encoding='utf-8') as stream:
+            try:
+                return yaml.load(stream=stream, Loader=yaml.CLoader)
+            except yaml.YAMLError as err:
+                raise err from err
